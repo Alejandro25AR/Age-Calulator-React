16cd9e65b911305b8fca7ea6dac32c835cb299ca
Alejandro25RA <alejito25raiders@gmail.com>
2023-07-05 19:01:11 -0500

    ⚡️ change useEffect for useMemo in useValidateForm

--- src/hooks/useValidateForm.ts
+++ src/hooks/useValidateForm.ts
@@ -1,15 +1,15 @@
-import { useEffect, useState } from "react";
+import { useMemo } from "react";
 // Models
 import { IDate,IFocusedFields,errorMessageEmpty } from "@/models";
 // Helpers
 import { helpValidateForm } from "@/helper";
 
 function useValidateForm(form:IDate,focusedFields:IFocusedFields) {
-  const [errorMessages,setErrorMessages] = useState({...errorMessageEmpty});
-  useEffect(() => {
+  const errorMessages = useMemo(() => {
+    const errorMessages = {...errorMessageEmpty};
     const errorNewMessages = helpValidateForm(form,focusedFields);
-    setErrorMessages({...errorMessages,...errorNewMessages});
-  },[form])
+    return {...errorMessages,...errorNewMessages};
+  },[form,focusedFields])
 
   return errorMessages;
 }
