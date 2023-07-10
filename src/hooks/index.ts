f79b67a0b89dd56544963ec360a7a07705687fa6
Alejandro25RA <alejito25raiders@gmail.com>
2023-07-10 10:15:43 -0500

    🏗️ add the new hooks in the index file

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,4 +1,7 @@
 export * from './useChangeTheme';
+export * from './useGetArrayDaysOfMonth';
+export * from './useGetNumberOfValidFields';
 export * from './useOnClickOutside';
 export * from './usePopup';
+export * from './useSubmit';
 export * from './useValidateForm';
