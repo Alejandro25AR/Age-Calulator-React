0f18ca222ee38d9fad8547585452a80803255c47
Alejandro25RA <alejito25raiders@gmail.com>
2023-07-09 19:47:00 -0500

    ♻️ change template EstimatedAge add props - Props year, month and day - Delete context AgeContext

--- src/components/templates/EstimatedAge/EstimatedAge.tsx
+++ src/components/templates/EstimatedAge/EstimatedAge.tsx
@@ -1,19 +1,21 @@
 import { TextPairs } from "@/components/molecules";
-import { AgeContext } from "@/context/AgeContext";
-import { useContext } from "react";
 
-function EstimatedAge() {
-  const { currentAge } = useContext(AgeContext);
-  
+interface Props {
+  year:string;
+  month:string;
+  day:string;
+}
+
+function EstimatedAge({ year,day,month }: Props) {
   return (
     <div className="t-estimated-age">
-    { currentAge &&
-      <> 
-        <TextPairs namePeriod="year"  value={currentAge.year}/>
-        <TextPairs namePeriod="month" value={currentAge.month}/>
-        <TextPairs namePeriod="day"   value={currentAge.day}/>
-      </>
-    }
+      {
+        <>
+          <TextPairs namePeriod="year" value={year} />
+          <TextPairs namePeriod="month" value={month} />
+          <TextPairs namePeriod="day" value={day} />
+        </>
+      }
     </div>
   );
 }
